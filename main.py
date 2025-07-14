527b43326e790ae8231a0e66e0d8fa9ee22662d2
HARSHAVARDHANGANGISHETTI <harshashetti11@gmail.com>
2025-07-14 13:07:51 +0530

    pdf meger

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfWriter
+merger = PdfWriter()
+pdfs=[]
+n = int(input("Enter the number of PDFs to merge: "))
+for i in range(0,n):
+    name = input(f"enter the name of PDF {i+1}:")
+    pdfs.append(name)
+for pdf in pdfs:
+    merger.append(pdf)    
+merger.write("merged.pdf")    
+merger.close()
+print("PDFs merged successfully into 'merged.pdf'")
